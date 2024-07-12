5552629c41d6cc11d0404b30f3dae3d0995d319d
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-07-12 09:49:00 -0300

    Quebrando um número

--- ex016.py
+++ ex016.py
@@ -3,4 +3,4 @@ num = float(input("Digite um valor:"))
 print("O valor digitado foi {} e a sua porção inteira é {} " .format(num, trunc(num)))'''
 
 num = float(input('Digite um valor: '))
-print('O valor digitado foi {} e a sua porção inteira é {}'.format)
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
