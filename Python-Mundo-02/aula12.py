643d70cdad7c9fd20e77940e78d0fdc1f59a9830
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-08 11:40:01 -0300

    Update aula12.py

--- Python-Mundo-02/aula12.py
+++ Python-Mundo-02/aula12.py
@@ -1,4 +1,11 @@
 nome = str(input('Qual é seu nome?'))
 if nome == 'David':
     print('Que nome top!')
-print('Tenha uma otima tarde {}'.format(nome))
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Geraldo':
+        print('Seu nome é bem popular no Brasil.')
+elif nome in 'Ana Clara Jéssica Julia':
+     print('Belo nome feminino')
+else:
+    print('Seu nome é bom normal ')
+    print('Tenha um bom dia, {}'.format(nome))
+
