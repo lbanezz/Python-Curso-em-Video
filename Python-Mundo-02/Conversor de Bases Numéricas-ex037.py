d9b786835990d4e24702fccae10de495ad175147
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-12 14:58:34 -0300

    Create Conversor de Bases Numéricas-ex037.py

---
+++ Python-Mundo-02/Conversor de Bases Numéricas-ex037.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um número inteiro: '))
+print('''Escolha uma das bases para coversão:
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+opção = int(input('Sua opção:'))
