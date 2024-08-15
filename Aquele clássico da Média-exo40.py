771517d65593b7a8bfdbdad9b3241c2b0f6a0503
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-15 10:47:50 -0300

    Create Aquele clássico da Média-exo40.py

---
+++ Aquele clássico da Média-exo40.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+média = (nota1 + nota2) / 2
+print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}' .format(nota1, nota2, média))
+if 7> média >=5:
+    print('O aluno está em Recuperação')
