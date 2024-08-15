9fbc19718c5cf0795b79d862cb70b581dc6c3412
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-15 13:27:59 -0300

    Update Aquele clássico da Média-exo40.py

--- Aquele clássico da Média-exo40.py
+++ Aquele clássico da Média-exo40.py
@@ -4,3 +4,4 @@ média = (nota1 + nota2) / 2
 print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}' .format(nota1, nota2, média))
 if 7> média >=5:
     print('O aluno está em Recuperação')
+    
