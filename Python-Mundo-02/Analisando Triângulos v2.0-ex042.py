1a72f8a40744b9e278707a16396f5930a625eab5
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-18 14:23:07 -0300

    Update Analisando Triângulos v2.0-ex042.py

--- Python-Mundo-02/Analisando Triângulos v2.0-ex042.py
+++ Python-Mundo-02/Analisando Triângulos v2.0-ex042.py
@@ -2,7 +2,8 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('oOs segmentos acima PODEM FORMAR triângulo!')
-    
+    print('Os segmentos acima PODEM FORMAR triângulo', end='')
+    if r1 == r2 and r2 == r3:
+        print('EQUILÁTERO!')
 else:
     print('Os segmentos acima NÃO PODEM FORMAR triângulo')
