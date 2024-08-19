d9006cda1f3b4e7fddb3cacbd3e8fb3e2a316b31
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-19 20:15:13 -0300

    Update Gerenciador de Pagamentos-ex044.py

--- Python-Mundo-02/Gerenciador de Pagamentos-ex044.py
+++ Python-Mundo-02/Gerenciador de Pagamentos-ex044.py
@@ -1,1 +1,8 @@
-print('')
+print('{}'.format('LOJAS REIS'))
+preco = float(input('Preço das comporas: R$'))
+print('''FORMAS DE PAGMENTO
+[ 1 ] à vista dinehiro/cheque
+[ 2 ] à vista no cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''' )
+opção = int(input('Qual é a opção?  '))
