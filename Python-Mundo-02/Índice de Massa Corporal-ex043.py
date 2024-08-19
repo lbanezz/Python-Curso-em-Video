1518268ad430790417f8bb4e15eac6a5befee86c
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-19 14:01:22 -0300

    Update Índice de Massa Corporal-ex043.py

--- Python-Mundo-02/Índice de Massa Corporal-ex043.py
+++ Python-Mundo-02/Índice de Massa Corporal-ex043.py
@@ -1,3 +1,14 @@
-peso = float(input('Qual é seu peso? (Kg)'))
-altura = float(input('Qual é sua altura? (m)'))
+peso = float(input('Qual é seu peso? (Kg) '))
+altura = float(input('Qual é sua altura? (m) '))
 imc = peso / (altura **2)
+print('O IMC dessa pessoa é de {:.1f}' .format((imc)))
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal')
+elif 18.5 <= imc < 25:
+    print('PARABENS, você está na faixa do PESO NORMAL')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDAD, cuidado! ')
+elif imc >= 40:
+    print('Você está em OBESIDADE MÓRBIDA, cuidado!')    
