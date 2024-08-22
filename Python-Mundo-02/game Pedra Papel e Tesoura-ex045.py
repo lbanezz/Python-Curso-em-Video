c30770ee814895929a653afe6b7de9dc2924deb9
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-22 13:18:59 -0300

    Update game Pedra Papel e Tesoura-ex045.py

--- Python-Mundo-02/game Pedra Papel e Tesoura-ex045.py
+++ Python-Mundo-02/game Pedra Papel e Tesoura-ex045.py
@@ -6,5 +6,35 @@ print('''Suas opções:
 [ 1 ] PAPEL
 [ 2 ] TESOURA ''')
 jogador = int(input('Qual é a sua jogada? '))
+print('-=' * 10)
 print('Computador jogou {}' .format(itens[computador]))
+print('-+' * 10)
 print('Jogador jogou {}' .format(itens[jogador]))
+if computador == 0: # computador jogou PEDRA 
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('Jogada inválida')
+elif computador == 1: # computador jogou PAPEL
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('Jogada inválida')
+elif computador == 2: # computador jogou TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada inválida')
+    
