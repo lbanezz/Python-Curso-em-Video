f404f60a97538779d052c23f48eb682c61a4d970
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-28 10:17:17 -0300

    Update Soma ímpares múltiplos de três-ex048.py

--- Python-Mundo-02/Soma ímpares múltiplos de três-ex048.py
+++ Python-Mundo-02/Soma ímpares múltiplos de três-ex048.py
@@ -1,3 +1,7 @@
+soma = 0
+cont = 0
 for d in range(1,501, 2):
-    if d % 3 ==0:
-        print(d, end='')
+    if d % 3 == 0:
+        cont = cont + 1
+        soma = soma + d
+print('A soma de todos os valores solicitados é {}'.format(cont, soma))
