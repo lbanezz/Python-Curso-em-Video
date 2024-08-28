a53f382bfba777effd5c1609410a6682789b7652
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-08-28 12:56:59 -0300

    Update Soma dos pares-ex050.py

--- Soma dos pares-ex050.py
+++ Soma dos pares-ex050.py
@@ -2,6 +2,7 @@ soma = 0
 cont = 0
 for d in range(1, 7):
     num = int(input('Digite o {} valor: '.format(d)))
-    soma  += num
-    cont += 1
+    if num % 2 == 0:
+        soma  += num
+        cont += 1
 print('Você informou {} números e a soma foi {}' .format(cont, soma))
