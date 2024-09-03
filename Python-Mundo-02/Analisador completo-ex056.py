21f32995da152b78378cbb4dcc803bf647ef6bd2
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-03 12:17:33 -0300

    Update Analisador completo-ex056.py

--- Python-Mundo-02/Analisador completo-ex056.py
+++ Python-Mundo-02/Analisador completo-ex056.py
@@ -1,7 +1,11 @@
 somaidade = 0
+médiaidade = 0
 for p in range ('1, 5'):
     print('------- {}ª PESSOA -----'.format(p))
     nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).strip()
-    somaidade = somaidade
+    somaidade += idade
+
+médiaidade = somaidade/ 4
+print('A média idade do grupo é de {} anos'.format(médiaidade))
