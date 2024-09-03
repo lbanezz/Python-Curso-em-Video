e50aad63e3e8ad49e598320d79f94f85ea0cf9e4
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-03 13:29:11 -0300

    Update Analisador completo-ex056.py

--- Python-Mundo-02/Analisador completo-ex056.py
+++ Python-Mundo-02/Analisador completo-ex056.py
@@ -2,6 +2,7 @@ somaidade = 0
 médiaidade = 0
 maioridadehomem = 0
 nomevelho = ''
+totmulher20= 0
 for p in range(1, 5):
     print('------- {}ª PESSOA -----'.format(p))
     nome = str(input('Nome: ')).strip()
@@ -12,8 +13,12 @@ for p in range(1, 5):
         maioridadehomem = idade
         nomevelho = nome
     if sexo in 'Mm' and idade > maioridadehomem:
-        maioridadehomem - idade
+        maioridadehomem = idade
         nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
 médiaidade = somaidade/ 4
 print('A média idade do grupo é de {} anos'.format(médiaidade))
-print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('O homem mais velho tem {} anos e se chama {}.' .format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos' .format(totmulher20))
+
