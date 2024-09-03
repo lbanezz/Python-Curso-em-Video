36949958a9dbd11a965ba47f3f4193ab80b67626
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-03 12:15:54 -0300

    Update Analisador completo-ex056.py

---
+++ Python-Mundo-02/Analisador completo-ex056.py
@@ -0,0 +1,7 @@
+somaidade = 0
+for p in range ('1, 5'):
+    print('------- {}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaidade = somaidade
