b099dd349d6f466cdf167a4729098c5fc0c47635
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-03 09:59:59 -0300

    Update Números primos-ex052.py

--- Python-Mundo-02/Números primos-ex052.py
+++ Python-Mundo-02/Números primos-ex052.py
@@ -1,3 +1,14 @@
 núm = int(input('Digite um número: '))
+tot = 0 
 for d in range(1, núm + 1):
-    print('{}', d, end='')
+    if núm % d == 0:
+        print('\033[36m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{}' .format(d), end='')
+print('\n\033 [m O número {} foi divisível {} vezes' .format(núm, tot))
+if tot == 2:
+    print('E por isso ele É PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
