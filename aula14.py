39cf719f61c4c47c1749b3a8078ff3008ea4fd5f
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-10 09:24:32 -0300

    Update aula14.py

--- aula14.py
+++ aula14.py
@@ -1,4 +1,5 @@
-for d in range(1, 10):
-    d = int(input('Digite um valor '))
-
+r = 'S'
+while r != 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar ? [S/N]')).upper()
 print('Fim')
