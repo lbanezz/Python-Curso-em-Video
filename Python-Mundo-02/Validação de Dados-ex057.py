b020e019cf78f9978d08f6b393848a89c0993166
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-11 11:05:33 -0300

    Update Validação de Dados-ex057.py

--- Python-Mundo-02/Validação de Dados-ex057.py
+++ Python-Mundo-02/Validação de Dados-ex057.py
@@ -1,4 +1,4 @@
-sexo = str(input('Informe seu sexo: [M/F]')).strip().upper()[0]
+sexo = str(input('Informe seu sexo: [M/F]  ')).strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = str(input('Dados iválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
 print('Sexo {} registrado com sucesso'.format(sexo))
