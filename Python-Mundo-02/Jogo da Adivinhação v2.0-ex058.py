64f0e4ea19e12ae42925ab18d7da0d982d81c13d
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-11 20:37:18 -0300

    Update Jogo da Adivinhação v2.0-ex058.py

--- Python-Mundo-02/Jogo da Adivinhação v2.0-ex058.py
+++ Python-Mundo-02/Jogo da Adivinhação v2.0-ex058.py
@@ -1,1 +1,10 @@
 from random import randint
+computador = randint(0,1)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue advinhar qual foi? ')
+acertou = False 
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    if jogador == computador:
+        acertou = True
+print('Acertou!')
