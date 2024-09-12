23edd6d3b3832bbf55bb557edf29ad09c176f0f9
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-12 09:41:28 -0300

    Update Criando um Menu de Opções-ex059.py

--- Python-Mundo-02/Criando um Menu de Opções-ex059.py
+++ Python-Mundo-02/Criando um Menu de Opções-ex059.py
@@ -1,9 +1,10 @@
 n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
-
-print(''' [ 1 ] somar
-[ 2 ] multiplicar
-[ 3 ] maior
-[ 4 ] novos números
-[ 5 ] sair do programa ''')
+opção = 0
+while opção != 5:
+    print('''  [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair do programa ''')
 opção = str(input('Qual é sua opção? '))
