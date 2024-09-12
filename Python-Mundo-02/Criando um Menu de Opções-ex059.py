c7eb9f8d01cc06cf9ddbaa40b4eec37d1b521952
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-12 10:28:20 -0300

    Update Criando um Menu de Opções-ex059.py

--- Python-Mundo-02/Criando um Menu de Opções-ex059.py
+++ Python-Mundo-02/Criando um Menu de Opções-ex059.py
@@ -7,4 +7,27 @@ while opção != 5:
     [ 3 ] maior
     [ 4 ] novos números
     [ 5 ] sair do programa ''')
-opção = str(input('Qual é sua opção? '))
+    opção = str(input('>>>>>>>>>> Qual é sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}'.format(n1, n2, soma))
+    elif opção == 2:
+        produto = n1 * n2 
+        print('A soma entre {} + {} é'.format(n1, n2, produto))
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior valor é {}'.format(n1, n2, maior))
+    elif opção == 4:
+        print('Informe os números novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tnte novamente')
+    print('=-=' * 10)
+print('Fim do programa! Volte sempre!')
