2c03a0a9308a83158446fa97335d24f44e4914bc
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-12 19:30:43 -0300

    Reinstaçando o venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\x397812\Documents\GitHub\Python-Curso-em-Video\.venv
+set VIRTUAL_ENV=C:\Users\lbanezz\Documents\GitHub\Python-Curso-em-Video\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
