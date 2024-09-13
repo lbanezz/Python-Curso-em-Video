8d4eb05bd4cd9f9932f055b91b2eb394f9655e1f
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-12 21:42:56 -0300

    Create Cálculo do Fatorial-ex060.py

---
+++ Python-Mundo-02/Cálculo do Fatorial-ex060.py
@@ -0,0 +1,7 @@
+from math import factorial
+d = int(input('Digite um númeor para calcular seu Fatorial: '))
+c = d
+while c > 0:
+    print('{}'.format(c), end='')
+    c -= 1
+#print('O fatorial de {} é {}' .format(n, f))
