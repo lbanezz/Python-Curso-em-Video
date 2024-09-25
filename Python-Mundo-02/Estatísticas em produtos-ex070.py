3d0c6f7bec5641866f3b6be1cf1957b2a1d69dbf
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-09-25 10:54:02 -0300

    Update Estatísticas em produtos-ex070.py

--- Python-Mundo-02/Estatísticas em produtos-ex070.py
+++ Python-Mundo-02/Estatísticas em produtos-ex070.py
@@ -1,10 +1,16 @@
-total = totmil = 0
+total = totmil = menor = cont = 0
 while True:
     produto = str(input('Nome do Porduto: '))
     valor = float(input('Valor: R$'))
+    cont += 1
     total += valor
     if valor > 1000:
         totmil += 1
+    if cont == 1:
+        menor = valor
+    else:
+        if valor < menor:
+            menor = valor
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
@@ -13,3 +19,4 @@ while True:
 print('{:-^40}'.format('FIM DO PROGRAMA'))
 print(f'O total da compra foi {total:.2f}')
 print(f'Temos {totmil} produtos cutando mais de R$1000.00')
+print(f'O produto mais barato custa R${menor:.2f}')
