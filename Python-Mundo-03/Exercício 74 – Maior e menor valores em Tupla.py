c3c0e2a9dc3a00d999e0d3426a8919f4b105f6dd
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-10-23 22:41:52 -0300

    Update Exercício 74 – Maior e menor valores em Tupla.py

--- Python-Mundo-03/Exercício 74 – Maior e menor valores em Tupla.py
+++ Python-Mundo-03/Exercício 74 – Maior e menor valores em Tupla.py
@@ -1,6 +1,9 @@
 from random import randint
 numeros = randint(1,10), randint(1,10), randint(1,10), 
 randint(1,10), randint(1,10),
-print('Os valores sorteados foram :')
+print('Os valores sorteados foram: ', end='')
 for n in numeros: 
     print(f'Eu sortei o valor {n}')
+print(f'O maior valor sorteado foi {max(numeros)}')
+print(f'O maior valor sorteado foi {min(numeros)}')
+
