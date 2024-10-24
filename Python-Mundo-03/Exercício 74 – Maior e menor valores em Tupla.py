bd694f692a334361b22452a813a88e0d0b67f47a
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-10-23 22:30:22 -0300

    Update Exercício 74 – Maior e menor valores em Tupla.py

--- Python-Mundo-03/Exercício 74 – Maior e menor valores em Tupla.py
+++ Python-Mundo-03/Exercício 74 – Maior e menor valores em Tupla.py
@@ -1,4 +1,4 @@
-from random import radint
-n = radint(1,10), radint(1,10), radint(1,10), 
-radint(1,10), radint(1,10),
+from random import randint
+n = randint(1,10), randint(1,10), randint(1,10), 
+randint(1,10), randint(1,10),
 print(f'Eu sorteei o valor {n}')
