cc57b4428595b34a7d057ca349a217ae48ea90bc
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-10-24 21:47:47 -0300

    Update Exercício 75 – Análise de dados em uma Tupla.py

--- Python-Mundo-03/Exercício 75 – Análise de dados em uma Tupla.py
+++ Python-Mundo-03/Exercício 75 – Análise de dados em uma Tupla.py
@@ -7,7 +7,7 @@ print(f'O valor 9 apareceu {num.count(9)} vezes')
 if 3 in num:
     print(f'O valor 3 apareceu na {num.index(3)} posição')
 else: 
-    print('O valor 3 não foi digitado em nenhuma posição')
+    print('O valor 3 não foi digitado em nenhjuma posição')
 print('Os valores pares digitados foram ', end='')
 for n in num: 
     if n % 2 == 0:
