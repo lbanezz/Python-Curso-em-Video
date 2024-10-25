8a21626493d15aa488ac21389d8533c549e8fde0
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-10-24 21:31:14 -0300

    Update Exercício 75 – Análise de dados em uma Tupla.py

---
+++ Python-Mundo-03/Exercício 75 – Análise de dados em uma Tupla.py
@@ -0,0 +1,6 @@
+num = (int(input('Digite um número: ')),
+        int(input('Digite putro número: ')),
+        int(input('Digite más número: ')),
+        int(input('Digite último número: ')))
+print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
