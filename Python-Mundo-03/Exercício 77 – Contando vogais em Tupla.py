9ab03834dfed01839adcdc3ff3d8dc074bb59f2b
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-10-27 15:06:43 -0300

    Update Exercício 77 – Contando vogais em Tupla.py

---
+++ Python-Mundo-03/Exercício 77 – Contando vogais em Tupla.py
@@ -0,0 +1,8 @@
+frases = ('aprender', 'programar', 'linguagem', 'python',
+          'curso', 'gratis', 'estudar', 'praticar',
+          'trabalhar', 'mercado', 'programador', 'futuro')
+for p in frases:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
