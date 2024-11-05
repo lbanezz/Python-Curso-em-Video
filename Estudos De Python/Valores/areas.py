69fa22f4cc2fec8a35d680ed37ff0d4040a270a2
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2024-11-04 22:24:31 -0300

    Create areas.py

---
+++ Estudos De Python/Valores/areas.py
@@ -0,0 +1,9 @@
+# Calculo da área de um círculo
+raio = 4
+pi = 3.14
+print('área do circulo' + str(pi * raio ** 2))
+
+'''Cálculo da área de um retângulo'''
+base = 7
+largura = 4
+print('área do retângulo' + str(base * largura))
