cfef918cbe5135f3b9bb553920a8d5543b73f565
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-01-24 07:24:10 -0300

    update

--- Python-Mundo-03/Exercício 85 – Listas com pares e ímpares.py
+++ Python-Mundo-03/Exercício 85 – Listas com pares e ímpares.py
@@ -8,6 +8,8 @@ for c in range(1, 8):
         núm[1].append(valor)
 
 print('-=' * 30)
+núm[0].sort()
+núm[1].sort()
 print(f'Os valores pares digitados foram: {núm[0]}')
 print(f'Os valores ímpares digitados foram: {núm[1]}')
 
