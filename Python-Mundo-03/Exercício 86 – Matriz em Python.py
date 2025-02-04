dbbfd4752bedd91d6b219cdb6cb1f95a9c435fe7
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-02-03 22:33:51 -0300

    Update Exercício 86 – Matriz em Python.py

--- Python-Mundo-03/Exercício 86 – Matriz em Python.py
+++ Python-Mundo-03/Exercício 86 – Matriz em Python.py
@@ -1,1 +1,6 @@
-metriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz [l] [c] = int(input(f'Digite um valor: '))
+
+print(matriz)
