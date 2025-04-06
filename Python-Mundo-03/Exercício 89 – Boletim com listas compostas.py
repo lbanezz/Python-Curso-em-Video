b58154a8f2a52f9622a3d7fd3b052f0a578af33d
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-04-06 19:06:12 -0300

    Update Exercício 89 – Boletim com listas compostas.py

--- Python-Mundo-03/Exercício 89 – Boletim com listas compostas.py
+++ Python-Mundo-03/Exercício 89 – Boletim com listas compostas.py
@@ -13,3 +13,12 @@ print(f'{'No.':<4}{'NOME':<10}{'MÉDIA':>8}')
 print('-' * 26)
 for i, a in enumerate(ficha):
     print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True: 
+    print('-' * 30)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe)'))
+    if opc == 999: 
+        print('Finalizando...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc] [0]} são {ficha[opc] [1]} ')
+print('<<< Volte sempre >>>>')
