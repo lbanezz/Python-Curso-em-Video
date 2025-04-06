3507a3f0c994cbada80a0941a3b88e504b2eef77
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-04-05 22:11:40 -0300

    Update Exercício 88 – Palpites para a Mega Sena.py

---
+++ Python-Mundo-03/Exercício 88 – Palpites para a Mega Sena.py
@@ -0,0 +1,27 @@
+from random import randint 
+from time import sleep
+lista = list()
+jogos = list()
+print('-' *  30)
+print('      Joga na mega sena         ')
+print('-' * 30)
+quant = int(input('Quantos  jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear
+    tot += 1
+print('-=' * 3, f'Sorteando {quant} jogos ', '-=' * 3)
+for i, l in enumerate(jogos):
+    sleep(2)
+    print(f' Jogos {i}: {l} ')
+print('-=' 5, '< boia sorte > f', '-=' * 5 )
