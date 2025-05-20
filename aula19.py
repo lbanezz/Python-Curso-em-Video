4565b47832138af751bc823b8c43105c9dd05a0b
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-05-19 22:36:24 -0300

    Update aula19.py

--- aula19.py
+++ aula19.py
@@ -1,1 +1,7 @@
-pessoas = {'nome': 'David'}
+estado = dict()
+brasil = list()
+for c in range(0,3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+print(brasil)
