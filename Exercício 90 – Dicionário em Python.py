0bd5ecc3b7f5d314188c3e60fe4f81abb04b6f0a
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-05-20 22:28:09 -0300

    Update Exercício 90 – Dicionário em Python.py

--- Exercício 90 – Dicionário em Python.py
+++ Exercício 90 – Dicionário em Python.py
@@ -3,3 +3,7 @@ aluno['nome'] = str(input('Nome: '))
 aluno['média'] = float(input(f'Média de {aluno['nome']}:'))
 if aluno['média'] >= 7:
     aluno['siatuação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['Situação'] = 'Reprovado'
+
+print(aluno)
